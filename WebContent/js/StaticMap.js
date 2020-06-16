619037e72c5530378d041f28d45bf8e7cca1006e
marco-blit <marcocaiocaiumi@gmail.com>
2020-06-16 18:27:06 +0200

    il mio lavoro qua è finito

--- WebContent/js/StaticMap.js
+++ WebContent/js/StaticMap.js
@@ -1,55 +1,50 @@
-let staticMapsInternal = []
-function CreateStaticMap(mapHTML, image)
-{
-    const map = {html:mapHTML, markers: []}
+var staticMapsInternal = []
+function CreateStaticMap(mapHTML, image) {
 
-    const imgMap = mapHTML.getElementsByTagName('img')[0]
+    var map = {html:mapHTML, markers: []}
+
+    var imgMap = mapHTML.getElementsByTagName('img')[0]
 
     imgMap.src = image
 
-    f = function (position, click)
-    {
+    f=function (position, click){
+    
         var marker = document.createElement('div');
         marker.classList.add('marker')
-        marker.style.top = position.y + "%"
         marker.style.left = position.x + "%"
-
-
-        marker.addEventListener('click', function()
-        {
-            click(marker)
-        })
+        marker.style.top = position.y + "%"
+        
+        marker.addEventListener('click',click)
 
         this.html.appendChild(marker);
 
         this.markers.push(marker)
     }
-    map.AddMarker = f.bind(map)
+    map.AddMarker=f.bind(map)
 
-    f = function()
-    {
-        this.markers.forEach(elem =>
-            {
-                const tmp = elem.style.left
+    z=function(){
+    
+        this.markers.forEach(function(elem){          
+                var tmp = elem.style.left
                 elem.style.left = elem.style.top
-                elem.style.top = elem.style.left
-            })
+                elem.style.top = tmp
+        })
     }
-
-    map.onorientationchange = f.bind(map)
-
+    map.onorientationchange=z.bind(map)
+    
     staticMapsInternal.push(map)
 
     return map
 }
-function RelativePosition(x,y)
-{
+
+function RelativePosition(x,y){
+
     return {x:x, y:y}
 }
 
-window.onorientationchange = function()
-{ 
-    staticMapsInternal.forEach(map => {
+window.addEventListener("onorientationchange",function(){
+
+    staticMapsInternal.forEach(function(map){
         map.onorientationchange()
     })
-}
+})
