271cd9dd88fa74154ffcbe4f36208f8292469665
pampinetor <66369529+pampinetor@users.noreply.github.com>
2020-06-17 10:12:13 +0200

    DEF

--- WebContent/js/Map.js
+++ WebContent/js/Map.js
@@ -4,8 +4,7 @@ function CreateMap(mapHTML)
     {
         center: {lat: 44.6888768, lng: 10.6594437},
         zoom: 14
-    })
-
+    }  )
     map.markers = []
     map.AddMarker = AddMarker.bind(map)
     map.userPos = new google.maps.Marker({
@@ -16,43 +15,29 @@ function CreateMap(mapHTML)
         map: map
     })
     map.watchId = navigator.geolocation.watchPosition(UpdatePos.bind(map))
-
     setInterval(CheckProximity.bind(map), 5000)
 
-    return map
-}
+    return map}
 
 function CheckProximity()
-{
-	this.markers.forEach(marker =>
-	{
-		
-	})
-	
-}
+{this.markers.forEach(marker =>{})
+	}
 function UpdatePos(position)
 {
     this.userPos.setPosition(
-        {
-            lat: position.coords.latitude,
-            lng: position.coords.longitude
-        })
+        {lat: position.coords.latitude,
+            lng: position.coords.longitude})
 }
 function AddMarker(title, pos, click)
-{
-    var marker = new google.maps.Marker({
+ {
+     var marker = new google.maps.Marker({
         position: pos,
         map: this,
         title: title
     })
     marker.addListener('click', function()
-    {
-        click(marker)
-    })
+    {click(marker)})
     this.markers.push(marker)
-}
-
+ }
 function Marker(latitude, longitude)
-{
-    return {lat: latitude, lng: longitude}
-}
+{return {lat: latitude, lng: longitude}}
